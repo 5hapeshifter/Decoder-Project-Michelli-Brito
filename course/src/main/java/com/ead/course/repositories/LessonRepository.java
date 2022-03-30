194bb185b49ff747e5771eda8e27252024b1661e
5hapeshifter <suleimandiverio@gmail.com>
2022-03-30 18:17:53 -0300

    modulo: Spring Data JPA: Do Básico ao Avançado, aula concluida: Criar RESTful para Lesson com Validações

--- course/src/main/java/com/ead/course/repositories/LessonRepository.java
+++ course/src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,5 +15,8 @@ public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 
     @Query(value="select * from tb_lessons where module_module_id = :moduleId", nativeQuery = true) // @Query só podemos utilizar quando for select, se for para deletar ou atualizar temos que usar @Modifying em conjunto com @Query
     List<LessonModel> findAllLessonsIntoModule(@Param("moduleId") UUID moduleId);
+
+    @Query(value = "select * from tb_lessons where module_module_id = :moduleId and lesson_id = :lessonId", nativeQuery = true)
+    Optional<LessonModel> findLessonIntoModule(@Param("moduleId") UUID courseId, @Param("lessonId") UUID moduleId);
 
 }
