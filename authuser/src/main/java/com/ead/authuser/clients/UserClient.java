db269efd9b132b6075e886a185d1bf4360f6c10c
5hapeshifter <suleimandiverio@gmail.com>
2022-07-06 15:26:30 -0300

    modulo: API Composition Pattern e Synchronous Communication, aula concluida: API Composition Pattern em Authuser Microservice – Parte 02

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,10 +1,15 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -20,22 +25,30 @@ public class UserClient {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    UtilsService utilsService;
+
     private String REQUEST_URI = "http://localhost:8082";
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
         // url do outro microservice
-        String url = REQUEST_URI + "/courses?userid=" + userId + "$page=" + pageable.getPageNumber() + "&size"
-                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+        String url = utilsService.createUrl(userId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
         try {
+            // Parametrizacao para receber o response
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            // envio da requisicao chamando o MS
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
             log.debug("Response Number of Elements: {}", searchResult.size());
         } catch (HttpStatusCodeException e) {
             log.error("Error request /courses {} ", e);
         }
         log.info("Ending request /courses userId {} ", userId);
-        return ;
+        return result.getBody();
     }
 
 }
