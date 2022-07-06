3a71523feb308d9222d253a5ef2210f798ef2102
5hapeshifter <suleimandiverio@gmail.com>
2022-07-06 18:03:32 -0300

    modulo: API Composition Pattern e Synchronous Communication, aula concluida: API Composition Pattern em Course Microservice

--- authuser/src/main/java/com/ead/authuser/services/impl/UtilsServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UtilsServiceImpl.java
@@ -12,7 +12,7 @@ public class UtilsServiceImpl implements UtilsService {
     private String REQUEST_URI = "http://localhost:8082";
 
     public String createUrl(UUID userId, Pageable pageable) {
-        return REQUEST_URI + "/courses?userid=" + userId + "$page=" + pageable.getPageNumber() + "&size"
+        return REQUEST_URI + "/courses?userid=" + userId + "&page=" + pageable.getPageNumber() + "&size"
                 + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
     }
 }
